661048002234bd3874f0b8c16f4bdeb99cf7c3c9
MoritzK177 <74679977+MoritzK177@users.noreply.github.com>
2021-09-16 22:58:33 +0200

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,20 @@
+//
+// Created by Moritz on 31.03.2021.
+//
+
+#ifndef FASTMARCHING_SETTINGS_H
+#define FASTMARCHING_SETTINGS_H
+
+
+
+namespace settings{
+    constexpr short x_grid_size {128};
+    constexpr short y_grid_size {128};
+    constexpr short z_grid_size {128};
+    constexpr int total_grid_size {x_grid_size*y_grid_size*z_grid_size};
+    constexpr double h{1.0/127};
+}
+
+
+
+#endif //FASTMARCHING_SETTINGS_H
