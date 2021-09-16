c39f5dc13912876a7fcd0af2888197102fc0b019
MoritzK177 <74679977+MoritzK177@users.noreply.github.com>
2021-09-16 23:02:01 +0200

    Update settings.h

--- settings.h
+++ settings.h
@@ -4,9 +4,6 @@
 
 #ifndef FASTMARCHING_SETTINGS_H
 #define FASTMARCHING_SETTINGS_H
-
-
-
 namespace settings{
     constexpr short x_grid_size {128};
     constexpr short y_grid_size {128};
@@ -14,7 +11,4 @@ namespace settings{
     constexpr int total_grid_size {x_grid_size*y_grid_size*z_grid_size};
     constexpr double h{1.0/127};
 }
-
-
-
 #endif //FASTMARCHING_SETTINGS_H
